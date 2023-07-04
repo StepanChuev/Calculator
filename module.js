a920a904f74f2a270a494ae68a3beaa48545c248
StepanChuev <133255497+StepanChuev@users.noreply.github.com>
2023-07-04 18:15:16 +0400

    Update module.js

--- module.js
+++ module.js
@@ -1,15 +1,31 @@
 'use strict';
 
 const normalize = (expression) => {
-	const operators = ["+", "-", "."];
+	const operators = ["+", "-", "*", "/", "^", "r", "."];
 	let normalized = "";
+	let onFractionalZero = false;
 
 	for (let i = 0; i < expression.length; i++){
-		if (expression[i] === " "){
+		const isOperator = operators.includes(expression[i]);
+
+		if (onFractionalZero && isOperator){
+			onFractionalZero = false;
+		}
+
+		if (expression[i] === " " || onFractionalZero){
 			continue;
 		}
 
-		if (!Number.isNaN(+expression[i]) || operators.includes(expression[i])){
+		else if (isOperator){
+			if (expression[i] === "." && (parseInt(expression.slice(i + 1)) === 0 || Number.isNaN(+expression[i + 1]) || expression[i + 1] === " ")){
+				onFractionalZero = true;
+				continue;
+			}
+
+			normalized += expression[i];
+		}
+
+		else if (!Number.isNaN(+expression[i])){
 			normalized += expression[i];
 		}
 	}
